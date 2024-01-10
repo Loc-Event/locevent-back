26ca7cc62053cf458203b0509e959d8cd0e7dc3c
Tai KHUN <taiyang78190@gmail.com>
2024-01-10 23:52:12 +0100

    flash message in header --DOING

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,6 +11,12 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
+        $this->addFlash("success", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem architecto fuga voluptatem illo nisi natus, veniam molestiae ");
+        // $this->addFlash("danger", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem architecto fuga voluptatem illo nisi natus, veniam molestiae ");
+        // $this->addFlash("warning", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem architecto fuga voluptatem illo nisi natus, veniam molestiae ");
+        // $this->addFlash("info", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem architecto fuga voluptatem illo nisi natus, veniam molestiae ");
+
+
         return $this->render('main/index.html.twig');
     }
 }
