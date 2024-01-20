3974847ce8a111b8c5659d2e312ea2f2b6a087ab
Tai KHUN <taiyang78190@gmail.com>
2024-01-20 20:50:06 +0100

    style form et traduction -DONE

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -29,7 +29,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
